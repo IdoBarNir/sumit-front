e27c068369196fcc363cd14faa353003befb7c98
idomiko <barnir.ido@gmail.com>
2023-08-30 21:22:33 +0300

    2nd

--- src/Pages/GamePage/GamePage.tsx
+++ src/Pages/GamePage/GamePage.tsx
@@ -151,9 +151,7 @@ const GamePage: FC<GamePageProps> = ({ setConclusion, setIsWin }) => {
           <Glass role="left" position={glassPositions} />
           <Glass role="right" position={glassPositions} />
         </Grid>
-        <Grid item>
-          <Typography variant="h6">Fill the glass without spilling</Typography>
-        </Grid>
+        <Typography variant="h6">Fill the glass without spilling</Typography>
         <Grid container justifyContent="space-evenly" style={{ width: "100%" }}>
           <Shaker
             label="A"
@@ -192,7 +190,6 @@ const GamePage: FC<GamePageProps> = ({ setConclusion, setIsWin }) => {
         ) : (
           <CircularProgress style={{ margin: 40 }} />
         )}
-
         <Grid item>
           <Typography variant="body2" color="error">
             {error}
